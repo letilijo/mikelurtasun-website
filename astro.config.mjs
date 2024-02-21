ebdfc7c1af94c0982cc3ff6c7eb418c68fb27419
Leticia Lijó <leticialijo@gmail.com>
2024-02-21 11:46:09 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://letilijo.github.io',
+  base: 'mikelurtasun-website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
